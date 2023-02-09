48e64d45b5818ad8aa4810c37ddb9c686d319bee
archita19 <madhusmitasamudra@gmail.com>
2023-02-10 01:33:22 +0530

    added filter search

--- server/models/bus.py
+++ server/models/bus.py
@@ -12,28 +12,35 @@ class Bus:
             return self.db.Bus.find({})
         except Exception as e:
             print(e)
-            return {}
+            return {'Message': 'Failed to find buses'}
 
     def find_a_bus(self, bus_id):
         try:
             return self.db.Bus.find_one({"_id": ObjectId(bus_id)})
         except Exception as e:
             print(e)
-            return {}
+            return {'Message': 'Failed to find the bus'}
 
     def search_bus(self, src, dst, day):
         try:
-            # return self.db.Bus.find({"start_city": src, "destination_city": dst, "routine": { "$elemMatch": {"day": day}}})
-            return self.db.Bus.find({"start_city": src, "destination_city": dst, "routine.day": day}, {"seat_price", "routine.arrival_time", "routine.departure_time" })
+            # return self.db.Bus.find({"start_city": src, "destination_city": dst, "routine": { "$elemMatch": {"day": day}}}, {"seat_price", "routine.arrival_time", "routine.departure_time"})
+            return self.db.Bus.find({"start_city": src, "destination_city": dst, "routine.day": day}, {"seat_price": 1, "routine.arrival_time": 1, "routine.departure_time": 1})
         except Exception as e:
             print(e)
-            return {}
+            return {'Message': 'Failed to search bus'}
+
+    def filter_search(self, filters):
+        try:
+            return self.db.Bus.find(filters, {"seat_price": 1, "routine.arrival_time": 1, "routine.departure_time": 1})
+        except Exception as e:
+            print(e)
+            return {'Message': 'Failed to apply filter'}
 
 if __name__ == "__main__":
     x = Bus()
     # print(list(x.find_all_buses()))
     # print(x.find_a_bus("63e4af4b219ec66d45d9b2d"))
-    print(list(x.search_bus("goa", "delhi", "friday")))
+    print(list(x.search_bus("goa", "delhi", "sunday")))
 
     
 
