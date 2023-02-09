31a5d3c3f5b4e1b44c70f74686dcba6b0aa0072b
Omkar Savoikar <omkarsavoikar@gmail.com>
2023-02-09 20:55:32 +0530

    Add view_all function for User

--- server/models/user.py
+++ server/models/user.py
@@ -1,7 +1,15 @@
+import sys
+sys.path.append("../")
 from DB.database import Database
 
 class User:
 	def __init__(self) -> None:
-		self.db = Database()
-		self.collection_name = "User"
+		self.db = Database().get_database()
 		
+	def view_all(self):
+		try:
+			table = self.db.User
+			cursor = table.find()
+			return cursor
+		except Exception as e:
+			return {}
