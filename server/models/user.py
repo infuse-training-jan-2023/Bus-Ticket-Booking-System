8f6ef8a1dbe7379dc76a266295ffb268548d7532
Omkar Savoikar <omkarsavoikar@gmail.com>
2023-02-09 16:22:35 +0530

    Add user constructor

--- server/models/user.py
+++ server/models/user.py
@@ -4,10 +4,4 @@ class User:
 	def __init__(self) -> None:
 		self.db = Database()
 		self.collection_name = "User"
-		self.fields = {
-			"emailid": "string",
-			"password": "string",
-			"gender": "string",
-			"is_admin": "boolean"
-		}
 		
