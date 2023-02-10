1caf524a94cb93423304e8b24a9e25fdcd4743b9
archita19 <madhusmitasamudra@gmail.com>
2023-02-10 13:29:48 +0530

    filter_search updated

--- server/models/bus.py
+++ server/models/bus.py
@@ -11,36 +11,51 @@ class Bus:
         try:
             return self.db.Bus.find({})
         except Exception as e:
-            print(e)
             return {'Message': 'Failed to find buses'}
 
     def find_a_bus(self, bus_id):
         try:
             return self.db.Bus.find_one({"_id": ObjectId(bus_id)})
         except Exception as e:
-            print(e)
             return {'Message': 'Failed to find the bus'}
 
-    def search_bus(self, src, dst, day):
-        try:
-            # return self.db.Bus.find({"start_city": src, "destination_city": dst, "routine": { "$elemMatch": {"day": day}}}, {"seat_price", "routine.arrival_time", "routine.departure_time"})
-            return self.db.Bus.find({"start_city": src, "destination_city": dst, "routine.day": day}, {"seat_price": 1, "routine.arrival_time": 1, "routine.departure_time": 1})
-        except Exception as e:
-            print(e)
-            return {'Message': 'Failed to search bus'}
-
+    # call the same function for search(src, dst, day)
     def filter_search(self, filters):
         try:
-            return self.db.Bus.find(filters, {"seat_price": 1, "routine.arrival_time": 1, "routine.departure_time": 1})
+            buses = self.db.Bus.find(filters)
+            res = []
+            for bus in buses:
+                x = {
+                    "start_city": bus["start_city"],
+                    "destination_city": bus["destination_city"],
+                    "seat_price": bus["seat_price"]
+                }
+                for routine in bus["routine"]:
+                    if routine["day"] == filters["routine.day"]:
+                        x["arrival_time"] = routine["arrival_time"]
+                        x["departure_time"] = routine["departure_time"]
+                res.append(x)
+            return res
         except Exception as e:
-            print(e)
             return {'Message': 'Failed to apply filter'}
 
-if __name__ == "__main__":
-    x = Bus()
+# if __name__ == "__main__":
+    # x = Bus()
     # print(list(x.find_all_buses()))
     # print(x.find_a_bus("63e4af4b219ec66d45d9b2d"))
-    print(list(x.search_bus("goa", "delhi", "sunday")))
+    # print(x.search_bus("goa", "delhi", "sunday"))
+    # filter = {
+    #     "start_city": "goa",
+    #     "destination_city": "delhi",
+    #     "routine.day": "sunday",
+    #     "seat_price": {
+    #         "$lte": 1500
+    #     },
+    #     "routine.arrival_time": {
+    #         "$gte": 1800
+    #     }
+    # }
+    # print(x.filter_search(filter))
 
     
 
