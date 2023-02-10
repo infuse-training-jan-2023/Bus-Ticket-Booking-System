c031ee38097adeeee0580315aec48563c0ae0e11
Omkar Savoikar <omkarsavoikar@gmail.com>
2023-02-10 12:21:07 +0530

    Update book-ticket feature to append selected seats in the bus

--- server/models/ticket.py
+++ server/models/ticket.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append("../")
 from DB.database import Database
+from bus import Bus
 
 class Ticket():
     def __init__(self):
         self.db = Database().get_database()
 
-    def book_ticket(self, bus_id, user_id, date, ticket_price, selected_seats):
+    def book_ticket(self, bus_id, user_id, ticket_price, date, selected_seats, day):
         try:
             table = self.db.Ticket
             cursor = table.insert_one({
@@ -17,11 +18,16 @@ class Ticket():
                 "selected_seats": selected_seats,
                 "status": True
             })
+            bus_data = Bus().add_selected_seats(bus_id, selected_seats, date, day)
             return {
                 "ticket_id": cursor.inserted_id,
                 "bus_id": bus_id,
                 "user_id": user_id,
                 "date": date,
+                "start_city": bus_data["start_city"],
+                "destination_city": bus_data["destination_city"],
+                "arrival_time": bus_data["arrival_time"],
+                "departure_time": bus_data["departure_time"],
                 "ticket_price": ticket_price,
                 "selected_seats": selected_seats,
                 "status": True
