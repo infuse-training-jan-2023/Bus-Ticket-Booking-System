83f09c43ce0c58887f097313912be1e63064ddf1
archita19 <madhusmitasamudra@gmail.com>
2023-02-11 17:19:16 +0530

    navbar and login component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/navbar'
+import Footer from './components/footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <NavBar/>
+      
+      <p>
+      It is a long established fact that a reader will be distracted by the readable content of a 
+      page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
+      normal distribution of letters, as opposed to using 'Content here, content here', making it look 
+      like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their 
+      default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+      Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      </p>
+
+      <Footer/>
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import {Container} from 'react-bootstrap'
+
+export default function Footer() {
+  return (
+    <Container fluid>
+        <footer class="d-flex flex-wrap justify-content-center my-4 py-4 border-top">
+            <p class="text-muted">&copy; Infuse Company, Inc</p>
+        </footer>
+    </Container>
+  );
+}
