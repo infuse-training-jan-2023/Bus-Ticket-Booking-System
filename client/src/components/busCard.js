532a3a170bd662b2cdffddc92f7a8f91bcec6aa1
archita19 <madhusmitasamudra@gmail.com>
2023-02-12 01:13:14 +0530

    search page UI complete

--- client/src/components/busCard.js
+++ client/src/components/busCard.js
@@ -4,19 +4,19 @@ import {Card, Col, Row, Button} from 'react-bootstrap'
 export default function BusCard() {
   return (
         <Card
-          className="mb-2"
-          style={{ width: '54rem', textAlign: 'center'}}
+          className="mb-2 p-4"
+          style={{textAlign: 'center'}}
         >
-          <Row className='justify-content-md-center'>
-            <Col className='justify-content-md-center'>
+          <Row className='justify-content-md-center align-items-center'>
+            <Col xs={8}>
                 <Row>
                     <Col sm>
                         <div>04:30PM</div>
                         <div>Panjim</div>
                     </Col>
                     <Col sm>
-                        <div>04:30PM</div>
-                        <div><img src='../../images/right-arrow.png' width={50} height={10} /></div>
+                        <div style={{fontStyle: 'italic', fontSize: '0.8rem', marginBottom: '0'}}>12hrs 15mins</div>
+                        <div><img src='../../images/right-arrow.png' width={150} height={20} alt="Arrow"/></div>
                     </Col>           
                     <Col sm>                
                         <div>12:30AM</div>
@@ -24,10 +24,10 @@ export default function BusCard() {
                     </Col>
                 </Row>
             </Col>
-            <Col sm className='m-2'>
-                <div>Rs. 1000</div>
+            <Col className='m-2'>
+                <div>Rs.1000</div>
                 <div>
-                    <Button variant="success">Book</Button>
+                    <Button variant="success" style={{width: '100%'}}>Book</Button>
                 </div>
             </Col>
           </Row>
