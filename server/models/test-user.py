aaa76a815e0b315be0a6d4811adca63d6ea2c967
aftab <aftab.saeedi2015@gmail.com>
2023-02-13 11:21:54 +0530

    refactored code

--- server/models/test-user.py
+++ server/models/test-user.py
@@ -12,30 +12,37 @@ class UnitTestCase(unittest.TestCase):
         }
         self.mock_error = {}
     @patch("pymongo.collection.Collection.find_one")
-    def test_register_existing_user(self, mock_find_one):
+    @patch("pymongo.collection.Collection.insert_one")
+    def test_register_existing_user(self,mock_insert_one, mock_find_one):
         # Test case when the user already exists in the database
         mock_find_one.return_value = self.mock_error
+        mock_insert_one.return_value = False
         user = User()
         result = user.register(self.mock_user_data)
         self.assertEqual({}, result)
-        mock_find_one.assert_called_once_with({"emailid": self.mock_user_data["emailid"]})
+        mock_find_one.assert_called_with({"emailid": self.mock_user_data["emailid"]})
 
     @patch("pymongo.collection.Collection.find_one")
-    def test_register_new_user(self, mock_find_one):
-        # Test case when the user does not exist in the database
-        mock_find_one.return_value = self.mock_user_data
+    @patch("pymongo.collection.Collection.insert_one")
+    @patch("models.user.User.fetch_user")
+    def test_register_new_user(self,mock_fetch_user,mock_insert_one, mock_find_one):
+        mock_find_one.return_value = False
+        mock_insert_one.return_value = True
+        mock_fetch_user.return_value = self.mock_user_data
         user = User()
         result = user.register(self.mock_user_data)
         self.assertEqual(self.mock_user_data, result)
-        mock_find_one.assert_called_once_with({"emailid": self.mock_user_data["emailid"]})
+        mock_find_one.assert_called_with({"emailid": self.mock_user_data["emailid"]})
 
     @patch("pymongo.collection.Collection.find_one")
-    def test_register_new_user_invalid_data(self,mock_find_one):
+    @patch("pymongo.collection.Collection.insert_one")
+    def test_register_new_user_invalid_data(self,mock_insert_one,mock_find_one):
         mock_find_one.return_value = {}
+        mock_insert_one.return_value = False
         user = User()
         result = user.register(self.mock_user_data)
         self.assertEqual({},result)
-        mock_find_one.assert_called_once_with({"emailid": self.mock_user_data["emailid"]})
+        mock_find_one.assert_called_with({"emailid": self.mock_user_data["emailid"]})
 
     # test login
     @patch("pymongo.collection.Collection.find_one")
