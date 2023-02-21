0ec5c3fbb47a560bf9eccf37e3096eb5f4dc26e6
Omkar Savoikar <omkarsavoikar@gmail.com>
2023-02-21 17:22:27 +0530

    Testing

--- server/encoder.py
+++
@@ -1,8 +0,0 @@
-import json
-from bson import ObjectId
-
-class Encoder(json.JSONEncoder):
-    def default(self, o: any) -> any:
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(o)
