5e81c05755d3700fd4b3f9beeb8b6d0ca2f20e30
Omkar Savoikar <omkarsavoikar@gmail.com>
2023-02-28 10:03:35 +0530

    EC2 changes

--- server/application.py
+++ server/application.py
@@ -4,17 +4,17 @@ from controllers.user.user_controller import user_controller
 from controllers.ticket.ticket_controller import ticket_controller
 from controllers.bus.bus_controller import bus_controller
 
-app = Flask(__name__)
-CORS(app)
+application = Flask(__name__)
+CORS(application)
 
-@app.route('/', methods = ['GET'])
+@application.route('/', methods = ['GET'])
 def welcome():
 	return "Hello World"
 
-app.register_blueprint(user_controller)
-app.register_blueprint(ticket_controller)
-app.register_blueprint(bus_controller)
+application.register_blueprint(user_controller)
+application.register_blueprint(ticket_controller)
+application.register_blueprint(bus_controller)
 
 if __name__ == '__main__':
-    app.run(debug=True,host = '0.0.0.0',port = 4000)
+    application.run(debug=True)
 
